0ce1efb899d0f28e2038c0e5f91f6e8293261915
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-19 12:44:19 +0500

    in this file i practice data types

---
+++ dataTypes.py
@@ -0,0 +1,24 @@
+
+# String
+name  = "M Hashim";
+
+# int
+age = 21
+
+# flot
+marks = 90.90
+
+# boolen
+isPassed = True
+
+print(name)
+print(age)
+print(marks)
+print(isPassed)
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(isPassed))
+
+
