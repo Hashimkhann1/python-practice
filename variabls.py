1d5eb2aaedfdc1d6a1bd6f5ad94f9f9b905b3cb5
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-19 13:04:33 +0500

    variable practice

---
+++ variabls.py
@@ -0,0 +1,11 @@
+
+# Variables do not need to be declared with any particular type
+
+# name is variable
+name = "M hashim"
+x , y , z = 'car' , 'bike' , 'boat'
+
+print(name)
+print(x)
+print(y)
+print(z)
