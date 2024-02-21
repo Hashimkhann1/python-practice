ffe3062102b3645f03423fe713eb637658d2ac79
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-21 12:48:54 +0500

    opreaters practice

---
+++ opreatersPy.py
@@ -0,0 +1,36 @@
+
+# Python Arithmetic Operators
+# + , - , / , * , % , ** , //
+
+a = 10
+b = 2
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a // b)
+print(13 // b)
+print(a * b)
+print(a ** b)
+print(a % b)
+
+print('')
+print('-------------------------')
+print('')
+
+
+# Python Comparison Operators
+# == , != , > , < , >= , <=
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+# Python Logical Operators
+# and , or , not
+
+# Python Membership Operators
+# in , not in
