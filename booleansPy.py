c4748463d46cad4e5fa68c7db2ef4ea94eb0bb72
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-21 12:48:36 +0500

    boolean practice

---
+++ booleansPy.py
@@ -0,0 +1,21 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 10;
+b = 6;
+
+
+name = "M Hashim"
+name2 = "Hashim"
+
+if a > b:
+    print("Yes " + str(a) + " is greater than " + str(b))
+else:
+    print("No " + str(a) + " is not greater than " + str(b))
+
+
+if name == name2:
+    print(" Yes" + name + " " + name2 + "same names")
+else:
+    print(" No" + name + " " + name2 + " are not same names")
