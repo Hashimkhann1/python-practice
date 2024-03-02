a17eedb70517a6fcefadebde0a0c96083c045b09
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-02 12:22:36 +0500

    sets practice

---
+++ setsPy.py
@@ -0,0 +1,15 @@
+mySet = {"apple", "banana", "cherry"}
+print(mySet)
+
+
+# loop set
+for x in mySet:
+  print(x)
+
+# add item
+mySet.add("orange")
+print(mySet)
+
+# remove item
+mySet.remove("orange")
+print(mySet)
