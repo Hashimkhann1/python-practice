f3f9a4996589feb1414d4407de474e7af83e42f0
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-21 23:45:13 +0500

    ehile loop practice

---
+++ whileLoop.py
@@ -0,0 +1,33 @@
+# //////// while loop ///////
+
+i = 1;
+
+# while i <= 10:
+#     print(i)
+#     i += 1;
+
+# ////// break Statement ///////
+# while i <= 6:
+#     print(i);
+#     if i == 3:
+#         print("loop is strop due to break statment")
+#         break
+#     i += 1;
+
+
+# ////// continue Statement ///////
+# while i <= 6:
+#     i += 1;
+#     if i == 3:
+#         print("continue")
+#         continue
+#     print(i);
+
+
+# printing message when condition is false
+
+while i < 6:
+      print(i)
+      i += 1
+else:
+      print("i is no longer less than 6")
